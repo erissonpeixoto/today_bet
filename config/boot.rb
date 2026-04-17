887301db7c7f1ab0238bfd42ae137d4cca93ef99
Erisson Peixoto <erisson06@gmail.com>
2026-04-16 21:55:33 -0300

    feat: load .env to bin/rails server

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,15 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+# Load .env file for local development (foreman/bin/dev loads it automatically,
+# but bin/rails server does not)
+env_file = File.expand_path("../../.env", __FILE__)
+if File.exist?(env_file)
+  File.foreach(env_file) do |line|
+    line = line.strip
+    next if line.empty? || line.start_with?("#")
+    key, value = line.split("=", 2)
+    ENV[key] ||= value
+  end
+end
