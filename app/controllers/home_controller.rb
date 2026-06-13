424337dd59c06bd18673e3d3d70db8c4e3cd708a
Erisson Peixoto <erisson06@gmail.com>
2026-06-13 09:55:17 -0300

    feat: matches yesterday and today

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @pagy, @matches = pagy(Match.order(:match_date), items: 12)
+    @pagy, @matches = pagy(Match.yesterday_and_today, items: 12)
     @top_users   = User.ranked.limit(5)
     @hottest_tip = Tip.order(
       Arel.sql("votes_agree_count + votes_hot_count + votes_disagree_count DESC")
